c2f07af99df8aa745d91354446ea6cafbacc1cc5
Jubayer Juhan <davidjuhan23@gmail.com>
2023-05-27 18:38:35 +0600

    Navbar Mobile Responsive

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import NAVLINKS from "@/constants/links/navlinks";
 import Button from "../Button/Button";
 import logo from "../../../assets/innovate-wave.svg";
 import styles from "./navbar.module.scss";
-
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
-      <Image src={logo} className={styles.logo} alt="Innovate Wave Logo" />
-      <ul className={styles.navlink__list}>
+      <Image src={logo} className={`${styles.logo}`} alt="Innovate Wave Logo" />
+      <MenuIcon
+        className={styles.menuIcon}
+        fontSize="large"
+        onClick={() => setOpen(true)}
+      />
+      <ul
+        className={`${styles.navlink__list} ${
+          open ? styles.navOpen : styles.hello
+        }`}
+      >
+        <CloseIcon
+          className={styles.closeIcon}
+          fontSize="large"
+          onClick={() => setOpen(false)}
+        />
         {NAVLINKS.map((navlink, index) => (
           <li className={styles.navlink} key={index}>
             {navlink.name}
