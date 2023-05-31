583aec5b376c1122675d2db48cab38fe685947e6
Jubayer Juhan <davidjuhan23@gmail.com>
2023-05-31 16:10:29 +0600

    Feature bullet error fixed import

--- src/components/sections/home/FeatureBullets/FeatureBullets.tsx
+++ src/components/sections/home/FeatureBullets/FeatureBullets.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./featureBullet.module.scss";
 import { Bullet } from "../FeatureWithImageSection/types";
-import IconAndTitleBullet from "@/components/bullets/IconAndTitleBullet/IconAndTitleBullet";
+import { IconBulletProps } from "@/components/bullets/IconAndTitleBullet/types";
+import TextBlock from "@/components/ui/Text/TextBlock/TextBlock";
 
 const FeatureBullets = ({
   bulletType,
@@ -27,3 +28,21 @@ const FeatureBullets = ({
 };
 
 export default FeatureBullets;
+
+const IconAndTitleBullet = ({ Icon, type, bullet }: IconBulletProps) => {
+  return (
+    <div className={styles.bullet}>
+      <div className={styles.iconWrapper}>
+        <Icon className={styles.icon} />
+      </div>
+      <div className={styles.bulletBody}>
+        {type !== "iconOnly" && (
+          <h6 className={styles.bulletTitle}>{bullet.title}</h6>
+        )}
+        <TextBlock className={styles.description}>
+          {bullet.description}
+        </TextBlock>
+      </div>
+    </div>
+  );
+};
