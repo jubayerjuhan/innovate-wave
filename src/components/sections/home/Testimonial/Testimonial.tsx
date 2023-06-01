341345efd2866886e18bacde8fac51f704e969bd
Jubayer Juhan <davidjuhan23@gmail.com>
2023-06-01 15:37:15 +0600

    Testimonial Slider Added

--- src/components/sections/home/Testimonial/Testimonial.tsx
+++ src/components/sections/home/Testimonial/Testimonial.tsx
@@ -3,17 +3,66 @@ import predefinedStyles from "../../../../styles/predefined.module.scss";
 import styles from "./testimonials.module.scss";
 import FeattureSubheading from "@/components/ui/FeatureSubheading/FeattureSubheading";
 import TestimonialCard from "@/components/cards/TestimonialCard/TestimonialCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Testimonial = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const items = [
+    { message: "Hello" },
+    { message: "world" },
+    { message: "bye" },
+    { message: "world" },
+    { message: "gello" },
+  ];
   return (
     <div className={predefinedStyles.sectionPadding}>
       <FeattureSubheading title="Testimonials" />
       <h2 className={styles.heading}>Valuable feedback from our clients</h2>
-      <div className={styles.testimonialContainer}>
-        <TestimonialCard />
-        <TestimonialCard />
-        <TestimonialCard />
-      </div>
+      <Slider
+        {...settings}
+        autoplay
+        dots={false}
+        autoplaySpeed={3000}
+        accessibility={true}
+      >
+        {items.map((item, key) => (
+          <div key={key} className={styles.testimonialWrapper}>
+            <TestimonialCard />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
